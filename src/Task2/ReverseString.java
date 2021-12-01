dd872b12b5359cdd00ab7674f9e049dd0a09709c
mpieron <mpieron@griddynamics.com>
2021-12-01 10:03:04 +0100

    Added reverseFirstLast function for ReverseString class

--- src/Task2/ReverseString.java
+++ src/Task2/ReverseString.java
@@ -1,12 +1,14 @@
 package Task2;
 
+import java.util.Arrays;
+
 public class ReverseString {
 //    Write 3 implementations (or more) for reversing string.
 //    Input:  String stringToRevers = "I'm going to go to school today morning."
 //    Output: ".gninrom yadot loohcs ot og ot gniog m'I"
 
     public static void main(String[] args) {
-        System.out.println(reverseFor("I'm going to go to school today morning."));
+        System.out.println(reverseFirstLast("I'm going to go to school today morning."));
     }
 
     public static String reverseFor(String sentence){
@@ -30,5 +32,16 @@ public class ReverseString {
 
     public static String reverseStringBuffer(String sentence){
         return new StringBuffer(sentence).reverse().toString();
+    }
+
+    public static String reverseFirstLast(String sentence){
+        int length = sentence.length();
+        char[] newSentence = new char[length];
+
+        for(int i=0; i<length/2; i++){
+            newSentence[length-1-i] = sentence.charAt(i);
+            newSentence[i] = sentence.charAt(length-1-i);
+        }
+        return String.valueOf(newSentence);
     }
 }
