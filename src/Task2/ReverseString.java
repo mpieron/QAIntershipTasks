0e61ad05b49c66bd89bc1f66a954978828ae4294
mpieron <mpieron@griddynamics.com>
2021-12-02 11:45:47 +0100

    Added new methods for ReverseString class and print outs

--- src/Task2/ReverseString.java
+++ src/Task2/ReverseString.java
@@ -1,14 +1,24 @@
 package Task2;
 
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
 
 public class ReverseString {
 //    Write 3 implementations (or more) for reversing string.
 //    Input:  String stringToRevers = "I'm going to go to school today morning."
 //    Output: ".gninrom yadot loohcs ot og ot gniog m'I"
 
+    static String sentence = "I'm going to go to school today morning.";
+
     public static void main(String[] args) {
-        System.out.println(reverseFirstLast("I'm going to go to school today morning."));
+        System.out.println(reverseFor(sentence));
+        System.out.println(reverseWhile(sentence));
+        System.out.println(reverseStringBuffer(sentence));
+        System.out.println(reverseFirstLast(sentence));
+        System.out.println(reverseStack(sentence));
+        System.out.println(reverseRecursion(sentence));
+        System.out.println(reverseQueue(sentence));
     }
 
     public static String reverseFor(String sentence){
@@ -43,5 +53,33 @@ public class ReverseString {
             newSentence[i] = sentence.charAt(length-1-i);
         }
         return String.valueOf(newSentence);
+    }
+
+    public static String reverseStack(String sentence){
+        Stack<Character> stack = new Stack<>();
+        String newSentence = "";
+
+        for(int i=0; i<sentence.length(); i++)
+            stack.push(sentence.charAt(i));
+        while(!stack.empty())
+            newSentence += stack.pop();
+        return  newSentence;
+    }
+
+    public static String reverseRecursion(String sentence){
+        if (sentence.length() < 1)
+            return "";
+        return sentence.charAt(sentence.length()-1) + reverseRecursion(sentence.substring(0,sentence.length()-1));
+    }
+
+    public static String reverseQueue(String sentence){
+        String newSentence = "";
+        Queue<Character> queue = new ArrayDeque<>();
+
+        for(int i=sentence.length()-1; i>-1; i--)
+            queue.add(sentence.charAt(i));
+        while(!queue.isEmpty())
+            newSentence += queue.poll();
+        return newSentence;
     }
 }
